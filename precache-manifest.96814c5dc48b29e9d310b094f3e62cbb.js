104d1697905f7b6246ff13dd53bc781667420c71
Avinash.Komaturi <aviavinash0464@gmail.com>
2019-05-11 03:16:15 +0530

    Updates

---
+++ precache-manifest.96814c5dc48b29e9d310b094f3e62cbb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc481548dfa006126e30",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/css/main.446b86ef.chunk.css"
+  },
+  {
+    "revision": "bc481548dfa006126e30",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/js/main.bc481548.chunk.js"
+  },
+  {
+    "revision": "ddbc6186ef3eaa4aa6b5",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/css/1.b8251c81.chunk.css"
+  },
+  {
+    "revision": "ddbc6186ef3eaa4aa6b5",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/js/1.ddbc6186.chunk.js"
+  },
+  {
+    "revision": "82b2128673fde9c96c75",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/js/runtime~main.82b21286.js"
+  },
+  {
+    "revision": "be2ed1511f856f57648ebb4d1c0a1b96",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/user-img-placeholder.be2ed151.jpeg"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "4fcdd97fadc3a1d9887f816f2aa67f1d",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/CircularStd-Medium.4fcdd97f.otf"
+  },
+  {
+    "revision": "9c0eb6c2e967eccd837da618bcbde91c",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-regular-400.9c0eb6c2.woff2"
+  },
+  {
+    "revision": "87f37798dcdf11a68de16eacfd566aa3",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-regular-400.87f37798.woff"
+  },
+  {
+    "revision": "065edea411671c8caceecb999354cc21",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-regular-400.065edea4.eot"
+  },
+  {
+    "revision": "2e562d4ebf15395f00bc738738f79291",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-regular-400.2e562d4e.ttf"
+  },
+  {
+    "revision": "ae990e80be9a9904db60b0d3d06adbc1",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-brands-400.ae990e80.woff2"
+  },
+  {
+    "revision": "52134b924fd61958f88323845deffc64",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-solid-900.52134b92.woff2"
+  },
+  {
+    "revision": "d6ff69c82cc37b323ceb5ff20224cc99",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-brands-400.d6ff69c8.woff"
+  },
+  {
+    "revision": "a6de637ca793ed8092597012d691a6a0",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-solid-900.a6de637c.woff"
+  },
+  {
+    "revision": "1010b72339fe3292aede3543e827b741",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-brands-400.1010b723.eot"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-brands-400.c6aef942.ttf"
+  },
+  {
+    "revision": "5dc39cf0aed41e1a5e9918bea7e8603d",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-regular-400.5dc39cf0.svg"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-solid-900.872545dd.ttf"
+  },
+  {
+    "revision": "a20e6745adc3184de8330b1f0579161f",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-solid-900.a20e6745.eot"
+  },
+  {
+    "revision": "c95f5f7a8923b8bb5034de9eb48901d7",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-brands-400.c95f5f7a.svg"
+  },
+  {
+    "revision": "56404b18f3d1a3eaeed35b43343cfab9",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/static/media/fa-solid-900.56404b18.svg"
+  },
+  {
+    "revision": "6ed7d1f2e5678b6ca9494d48b559d633",
+    "url": "/AvinashKomaturi/Online_Retail_Dashboard/index.html"
+  }
+];
